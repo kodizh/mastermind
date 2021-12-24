41cfc26a43d28ab5ac5d83e399c5aac10a73eb7f
Nikolaz <kodizh@gouelier>
2021-12-24 11:47:45 +0100

    Add small coverage of integration tests
    The coverage validates two cases of playing the games by adding player guesses and
    checking out the modification of the MastermindCore properties.

---
+++ tests/integration/test_mastermind_core.py
@@ -0,0 +1,51 @@
+from mastermind.mastermind_core import MastermindCore
+
+
+def test_add_valid_guesses():
+    """ Validating adding a valid guess. The properties shall be modified
+        accordingly.
+    """
+    guess1 = ['Red', 'Green', 'Blue', 'Yellow']
+    guess2 = ['Red', 'Green', 'Blue', 'Blue']
+
+    mastermind = MastermindCore()
+    mastermind.configure(
+        code_length = 4,
+        colours_set = ['Red', 'Green', 'Blue', 'Yellow']
+    )
+    mastermind.secret_code = ['Green', 'Red', 'Blue', 'Blue']
+
+    ret = mastermind.add_guess(guess1)
+    assert ret == False
+    assert len(mastermind.player_guesses) == 1
+    assert mastermind.player_guesses[0]['guess'] == guess1
+    assert mastermind.player_guesses[0]['matching_places'] == 1
+    assert mastermind.player_guesses[0]['matching_colours'] == 2
+
+    ret = mastermind.add_guess(guess2)
+    assert ret == False
+    assert len(mastermind.player_guesses) == 2
+    assert mastermind.player_guesses[1]['guess'] == guess2
+    assert mastermind.player_guesses[1]['matching_places'] == 2
+    assert mastermind.player_guesses[1]['matching_colours'] == 2
+
+
+def test_add_winning_guess():
+    """ Validating adding a winning guess. The properties shall be modified
+        accordingly.
+    """
+    guess = ['Green', 'Red', 'Blue', 'Blue']
+
+    mastermind = MastermindCore()
+    mastermind.configure(
+        code_length = 4,
+        colours_set = ['Red', 'Green', 'Blue', 'Yellow']
+    )
+    mastermind.secret_code = ['Green', 'Red', 'Blue', 'Blue']
+    ret = mastermind.add_guess(guess)
+
+    assert ret == True
+    assert len(mastermind.player_guesses) == 1
+    assert mastermind.player_guesses[0]['guess'] == guess
+    assert mastermind.player_guesses[0]['matching_places'] == 4
+    assert mastermind.player_guesses[0]['matching_colours'] == 0
